ff4ebb3e6cf348dcc8c9cf3ff05d4b717020e81b
ricarellano <rarellanob@gmail.com>
2020-09-21 13:22:41 -0700

    change date format

--- src/actions/ActionSearch.js
+++ src/actions/ActionSearch.js
@@ -4,6 +4,7 @@ import Calendar from "../db/calendar.json";
 import Dropbox from "../db/dropbox.json";
 import Slack from "../db/slack.json";
 import Tweet from "../db/tweet.json";
+import { dateFormat, relativeDate } from "./utils";
 
 // Get Data from json files
 export const getData = (search, type = "all") => {
@@ -11,30 +12,35 @@ export const getData = (search, type = "all") => {
 
   if (type === "contacts" || type === "all") {
      filtered.contacts = Contacts.contacts.filter((data) => {
+      data.last_contact = dateFormat(data.last_contact);
       return data.matching_terms.includes(search);
     });
   }
 
   if (type === "calendar" || type === "all") {
     filtered.calendar = Calendar.calendar.filter((data) => {
+      data.date = dateFormat(data.date);   
       return data.matching_terms.includes(search);
     });
   }
 
   if (type === "dropbox" || type === "all") {
     filtered.dropbox = Dropbox.dropbox.filter((data) => {
+      data.created = dateFormat(data.created);
       return data.matching_terms.includes(search);
     });
   }
 
   if (type === "slack" || type === "all") {
     filtered.slack = Slack.slack.filter((data) => {
+      data.timestamp = relativeDate(data.timestamp);
       return data.matching_terms.includes(search);
     });
   }
 
   if (type === "tweet" || type === "all") {
     filtered.tweet = Tweet.tweet.filter((data) => {
+      data.timestamp = relativeDate(data.timestamp);
       return data.matching_terms.includes(search);
     });
   }
